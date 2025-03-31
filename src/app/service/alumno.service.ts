75d31454661f555b756c071cb501c21beff5786f
Valentino Burioni <118487875+VaalentinoB@users.noreply.github.com>
2025-03-31 17:48:59 -0300

    update fin

--- src/app/service/alumno.service.ts
+++ src/app/service/alumno.service.ts
@@ -6,7 +6,7 @@ import { Alumno } from '../model/alumno.model/alumno.model.component';
 })
 export class AlumnoService {
   private alumnos: Alumno[] = [
-    { id: 1, nombre: 'Juan Pérez', edad: 20, aprobado: true },
+    { id: 1, nombre: 'Valentino Burioni', edad: 20, aprobado: true },
     { id: 2, nombre: 'Ana García', edad: 22, aprobado: false },
     { id: 3, nombre: 'Carlos López', edad: 21, aprobado: true },
     { id: 4, nombre: 'María Rodríguez', edad: 23, aprobado: false },

52b2a9ec06dfd227b57cf29f79853b98dd342cb9
Valentino Burioni <118487875+VaalentinoB@users.noreply.github.com>
2025-04-08 11:38:36 -0300

    update: creando form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './components/lista-alumno';
 import { CommonModule } from '@angular/common';
 import { AprobadoDirective } from './directives/aprobado.directive';
+import { AlumnoFormComponent } from './components/alumno-form/alumno-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaAlumnosComponent,
+    AlumnoFormComponent,
     
   ],
   imports: [
     BrowserModule,  
     CommonModule,
-    AprobadoDirective
+    AprobadoDirective,
+    ReactiveFormsModule,  
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/alumno-form/alumno-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumno-form',
+  standalone: false,
+  templateUrl: './alumno-form.component.html',
+  styleUrl: './alumno-form.component.css'
+})
+export class AlumnoFormComponent {
+
+}

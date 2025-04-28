9512bd707108c2b0771cbf27acad8d413b507812
Valentino Burioni <valentinoburioni@hotmail.com>
2025-04-28 17:50:33 -0300

    fix errors

--- src/app/layout/toolbar/toolbar.component.ts
+++ src/app/layout/toolbar/toolbar.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   standalone: false,
   template: `
     <mat-toolbar color="primary">
+      <button mat-icon-button (click)="toggleSidebar.emit()">
+        <mat-icon>menu</mat-icon>
+      </button>
       <span>Administración de Alumnos - SIU</span>
     </mat-toolbar>
   `,
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Output() toggleSidebar = new EventEmitter<void>();
+}
